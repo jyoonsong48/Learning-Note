8357edacac72b462b70473422451d3d46664c1c6
Jiyoon Song <jyoon.song48@gmail.com>
2026-04-10 19:08:31 +0100

    Add files via upload

---
+++ BfB/2.13/farm.sh
@@ -0,0 +1,16 @@
+animal=$1
+
+if [[ $animal == "cow" ]]
+then
+    echo "Here, moo!"
+elif [[ $animal == "sheep" ]]
+then
+    echo "There a baa"
+elif [[ $animal == "duck" ]]
+then
+    echo "Everywhere a quack"
+else
+    echo "Old MacDonald had a farm"
+fi
+
+# SPACE!!!
